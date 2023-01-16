666dea38066d81a9290f8defe4027a12ec1ce3d4
Vi-user <vi.volkova.2712@gmail.com>
2023-01-16 23:57:36 +0300

    feat: add CRUD methods

--- async-race/src/api.ts
+++ async-race/src/api.ts
@@ -2,29 +2,71 @@ import { PAGES, Car } from './types/types';
 
 const BASE_URL = 'http://127.0.0.1:3000';
 
-// enum methods {
-//   'GET' = 'GET',
-//   'POST' = 'POST',
-//   'PATCH' = 'PATCH',
-//   'DELETE' = 'DELETE',
-// }
-
 export const garagePageUrl = `${BASE_URL}/${PAGES.GARAGE}`;
 
-export type getCarsParams = {
+enum Methods {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
+export type GetCarsParams = {
   page: number;
   limit: number;
-}
+};
 
-export type getCarsResponse = {
+export type GetCarsResponse = {
   carsData: Car[];
   carsQuantity: string | null;
-}
+};
 
-export const getCarsOnPage = async ({ page, limit = 7 }: getCarsParams): getCarsResponse => {
-// export const getCarsOnPage = async () => {
-  const response = await fetch(garagePageUrl);
+export const getCarsOnPage = async ({
+  page,
+  limit = 7,
+}: GetCarsParams): Promise<GetCarsResponse> => {
+  const response = await fetch(`${garagePageUrl}?page=${page}&_limit=${limit}`);
   const carsData = await response.json();
-  const carsQuantity = await response?.headers?.get('X-Total-Count');
+  const carsQuantity = response?.headers?.get('X-Total-Count');
   return { carsData, carsQuantity };
+};
+
+export const getCar = async (id: number): Promise<Car> => {
+  const response = await fetch(`${garagePageUrl}/${id}`);
+  const carData = await response.json();
+  return carData;
+};
+
+export const addCar = async ({ name, color }: Car): Promise<Car> => {
+  const response = await fetch(`${garagePageUrl}?name=${name}&color=${color}`, {
+    method: Methods.POST,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, color }),
+  });
+  const newCar = await response.json();
+  return newCar;
+};
+
+export const updateCar = async ({ name, color, id }: Car): Promise<Car> => {
+  const response = await fetch(`${garagePageUrl}/${id}`, {
+    method: Methods.PUT,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, color }),
+  });
+  const updatedCar = await response.json();
+  return updatedCar;
+};
+
+export const deleteCar = async (id: number): Promise<void> => {
+  // try {
+    const response = await fetch(`${garagePageUrl}/${id}`, {
+      method: Methods.DELETE,
+    });
+    const responseStatus = await response.json();
+    return responseStatus;
+  // } catch (err) { throw new Error(`${err}`);}
 };
